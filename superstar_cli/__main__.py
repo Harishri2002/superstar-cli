821da8d69ea41efc3f2309c703eae80570c63888
Harishri2002 <harishribr4@gmail.com>
2025-08-16 22:54:43 +0530

    Version 1.0.1 updated and deployed

--- superstar_cli/__main__.py
+++
@@ -1,9 +0,0 @@
-# superstar_cli/__main__.py
-
-from .animation import run
-
-def main():
-    run()
-
-if __name__ == "__main__":
-    main()
--- superstar_cli/animation.py
+++ superstar_cli/animation.py
@@ -1,3 +1,5 @@
+# superstar_cli/animation.py
+
 import time
 import os
 import sys
@@ -24,7 +26,7 @@ def hide_cursor(): sys.stdout.write("\033[?25l"); sys.stdout.flush()
 def show_cursor(): sys.stdout.write("\033[?25h"); sys.stdout.flush()
 
 # --- ASCII Art & Trace Path Definitions ---
-DOT_ART = { 'S':[" ●●●","●   "," ●●●","   ●","●●● "],'U':["●  ●","●  ●","●  ●","●  ●"," ●●●"],'P':["●●● ","●  ●","●●● ","●   ","●   "],'E':["●●●●","●   ","●●● ","●   ","●●●●"],'R':["●●● ","●  ●","●●● ","● ● ","●  ●"],'T':["●●●●●","  ●  ","  ●  ","  ●  ","  ●  "],'A':[" ●●● ","●   ●","●●●●●","●   ●","●   ●"],' ':["     ","     ","     ","     ","     "] }
+DOT_ART = { 'S':[" ●●●","●   "," ●●●","   ●","●●● "],'U':["●  ●","●  ●","●  ●","●  ●"," ●●●"],'P':["●●● ","●  ●","●●● ","●   ","●   "],'E':["●●●●","●   ","●●● ","●   ","●●●●"],'R':["●●● ","●  ●","●●●●","● ● ","●  ●"],'T':["●●●●●","  ●  ","  ●  ","  ●  ","  ●  "],'A':[" ●●● ","●   ●","●●●●●","●   ●","●   ●"],' ':["     ","     ","     ","     ","     "] }
 TRACE_ORDER = { 'S':[(1,0),(2,0),(3,0),(0,1),(1,2),(2,2),(3,2),(4,3),(3,4),(2,4),(1,4),(0,4)], 'U':[(0,0),(0,1),(0,2),(0,3),(0,4),(1,4),(2,4),(3,4),(4,4),(4,3),(4,2),(4,1),(4,0)], 'P':[(0,4),(0,3),(0,2),(0,1),(0,0),(1,0),(2,0),(3,0),(4,1),(3,2),(2,2),(1,2)], 'E':[(4,0),(3,0),(2,0),(1,0),(0,0),(0,1),(0,2),(0,3),(0,4),(1,4),(2,4),(3,4),(4,4),(0,2),(1,2),(2,2)], 'R':[(0,4),(0,3),(0,2),(0,1),(0,0),(1,0),(2,0),(3,0),(4,1),(3,2),(2,2),(1,2),(2,3),(3,4)], 'T':[(0,0),(1,0),(2,0),(3,0),(4,0),(2,1),(2,2),(2,3),(2,4)], 'A':[(0,4),(0,3),(0,2),(1,0),(2,0),(3,0),(4,1),(4,2),(4,3),(4,4),(0,2),(1,2),(2,2),(3,2)] }
 SOLID_ART = { 'R':["███ ","█  █","███ ","█ █ ","█  █"],'A':[" ██ ","█  █","████","█  █","█  █"],'J':["   █","   █","   █","█  █"," ██ "],'N':["█  █","██ █","█ ██","█  █","█  █"],'I':["███"," █ "," █ "," █ ","███"],' ':["    ","    ","    ","    ","    "] }
 
@@ -37,12 +39,10 @@ def get_text_width(text, art_dict):
             width += 5
     return width
 
-# --- Unified Renderer ---
 def render_frame(canvas_width, top_coords={}, middle_coords={}, bottom_coords={}, glow_sets={}):
     height = 22
     padding = (os.get_terminal_size().columns - canvas_width) // 2
     if padding < 0: padding = 0
-
     output = ""
     for y in range(height):
         line = ""
@@ -50,7 +50,6 @@ def render_frame(canvas_width, top_coords={}, middle_coords={}, bottom_coords={}
             top_char = f"{Colors.BRIGHT_BLUE if (x,y) in glow_sets.get('top', set()) else Colors.BLUE}●"
             middle_char = f"{Colors.BRIGHT_YELLOW if (x,y) in glow_sets.get('middle', set()) else Colors.YELLOW}█"
             bottom_char = f"{Colors.BRIGHT_BLUE if (x,y) in glow_sets.get('bottom', set()) else Colors.BLUE}●"
-
             if (x, y) in top_coords: line += top_char
             elif (x, y) in middle_coords: line += middle_char
             elif (x, y) in bottom_coords: line += bottom_char
@@ -58,13 +57,11 @@ def render_frame(canvas_width, top_coords={}, middle_coords={}, bottom_coords={}
         output += " " * padding + line + '\n'
     clear_screen(); sys.stdout.write(output + Colors.RESET); sys.stdout.flush()
 
-# --- Animation Functions ---
 def trace_word(text_to_draw, y_offset, existing_dots, canvas_width):
     newly_drawn_dots = set()
     cursor_x = 0
     word_width = get_text_width(text_to_draw, DOT_ART)
     x_offset = (canvas_width - word_width) // 2
-
     for char in text_to_draw:
         if char in TRACE_ORDER:
             for (px, py) in TRACE_ORDER[char]:
@@ -80,11 +77,7 @@ def animate_rajni_reveal(top_coords, bottom_coords, canvas_width):
     revealed_coords = set()
     cursor_x = 0
     word_width = get_text_width(text_middle, SOLID_ART)
-
-    # --- THE ONLY CHANGE IS HERE ---
-    # Manually nudged the text 2 spaces to the right for perfect visual alignment.
     x_offset = (canvas_width - word_width) // 2 + 2
-
     for char in text_middle:
         if char != ' ':
             art = SOLID_ART.get(char, SOLID_ART[' '])
@@ -94,10 +87,8 @@ def animate_rajni_reveal(top_coords, bottom_coords, canvas_width):
                         revealed_coords.add((cursor_x + x + x_offset, y + y_offset))
             render_frame(canvas_width, top_coords, revealed_coords, bottom_coords)
             time.sleep(RAJNI_SPEED)
-        
         if char in SOLID_ART: cursor_x += len(SOLID_ART.get(char)[0])
         else: cursor_x += 5
-
     return revealed_coords
 
 def animate_glow(top_coords, middle_coords, bottom_coords, canvas_width):
@@ -110,11 +101,14 @@ def animate_glow(top_coords, middle_coords, bottom_coords, canvas_width):
         render_frame(canvas_width, top_coords, middle_coords, bottom_coords, glow_sets)
         time.sleep(GLOW_SPEED)
 
-# --- Run the Animation ---
-if __name__ == "__main__":
+# --- This is the new main function that the command will call ---
+def main():
     try:
         try:
-            playsound.playsound('./assets/Rajani-8Bit.mp3', False)
+            # Note: The path needs to be found relative to this file
+            script_dir = os.path.dirname(__file__)
+            music_path = os.path.join(script_dir, 'assets', 'Rajani-8Bit.mp3')
+            playsound.playsound(music_path, False)
         except Exception as e:
             print(f"Could not play music. Error: {e}\nAnimation will continue without sound.")
             time.sleep(3)
